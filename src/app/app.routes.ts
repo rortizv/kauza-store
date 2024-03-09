b26e46fdd314eedb7d5f94ae8e91c822902dc792
Rafael Ortiz <rortiz@inkremental.co>
2024-03-09 12:35:27 -0500

    Add Tailwind, create components and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'products',
+    loadChildren: () => import('./features/products/products.component')
+  },
+  {
+    path: 'product-details/:id',
+    loadChildren: () => import('./features/products/details/details.component')
+  },
+  {
+    path: 'checkout',
+    loadChildren: () => import('./features/checkout/checkout.component')
+  },
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
+  { path: '**', redirectTo: 'products', pathMatch: 'full' }
+];
