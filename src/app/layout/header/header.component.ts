c24b56a854430336594570221f652957e546ad1a
Rafael Ortiz <rortiz@inkremental.co>
2024-03-10 12:34:31 -0500

    Hide/show cart resume on cart icon hover

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgClass],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  showCart = signal<boolean>(false);
 }
